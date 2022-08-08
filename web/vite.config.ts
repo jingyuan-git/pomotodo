86c4cf2ef47478525d8851af041002187532cd4f
jingyuan <lujingyuan95@gmail.com>
2022-08-08 10:10:58 +0800

    fix: bugfix for vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig(({ command, mode }: ConfigEnv) => {
       },
       extensions: ['.js', '.json', '.ts'] // 使用路径别名时想要省略的后缀名，可以自己 增减
     },
-    devServer: {
+    server: {
       proxy: {
         '/api': {
           target: env["VITE_APP_BASE_URL"],  // 
